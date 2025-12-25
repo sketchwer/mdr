070d7a1c174382723faafcf4530a884d344df345
raveenaku <charusharma3261@gmail.com>
2025-12-25 11:40:14 +0530

    dxdcvbnmdsadasddsdadasf,dsa

--- src/components/IndustryDetail/IndustryDetail.js
+++ src/components/IndustryDetail/IndustryDetail.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
 import Testimonials from '../Testimonials/Testimonials';
 import CTA from '../CTA/CTA';
 import './IndustryDetail.css';
